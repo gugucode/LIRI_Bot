72f73e889be479b3beaf8152f34a4f6f5c5f0f58
gugucode <meiyuechang@gmail.com>
2018-06-12 15:22:17 -0500

    add log.txt function

--- liri.js
+++ liri.js
@@ -3,11 +3,26 @@ var colors = require('colors');
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-
-// console.log(keys.twitter)
+var request = require("request");
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+// function appendlog(d){
+//     fs.appendFile("log.txt",d,(error)=>{
+//         if(error) throw error;
+//     });
+// }
+// overwrite console.log function
+console.log = function(d) {
+    d = d + '\n';
+    fs.appendFile("log.txt",d,(error)=>{
+        if(error) throw error;
+    });
+    process.stdout.write(d);
+  };
+
+// get my tweets from my twitter account
 function getTweets(){
     client.get('statuses/user_timeline', function(error, tweets, response) {
         if(error){
@@ -24,12 +39,13 @@ function getTweets(){
     });
 }
 
+// search song from sporify (limit 3 songs)
 function spotifySong(song){
-    spotify.search({ type: 'track', query: song, limit: 3 }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
+    spotify.search({ type: 'track', query: song, limit: 3 }, function(error, data) {
+        if (error) {
+          return console.log('Error occurred: ' + error);
         }
-            
+        
         for(var i = 0; i < data.tracks.items.length; i++){
             var song_info = data.tracks.items[i]
             console.log("----------------")
@@ -37,31 +53,77 @@ function spotifySong(song){
             console.log("Song's name: ".green + song_info.name);
             console.log("Preview url: ".green + song_info.preview_url);
             console.log("Album: ".green + song_info.album.name); 
-
         }
        
     });
 }
 
-var command = process.argv[2];
-switch(command){
-    case "my-tweets":
-        getTweets();
-        break;
-    case "spotify-this-song":
-        if(process.argv.length == 4){
-            spotifySong(process.argv[3]);
-        }else{
-            console.log("Please give me the name of the song!")
+// use request to get movie info from omdb API
+function movieThis(movie){
+    
+    var url = "http://www.omdbapi.com/?apikey=trilogy&t="+movie;
+    request(url,(error, response, body)=>{
+        if(error){
+            return console.log('Error occurred: ' + error);
         }
-        break;
-    case "movie-this":
-        movieThis();
-        break;
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
-    default:
-        console.log("Sorry, I don't understand.")
-        break;
+        
+        var movie_detail = JSON.parse(body);
+        console.log("Title: ".green + movie_detail.Title);
+        console.log("Year: ".green + movie_detail.Year);
+        console.log("IMDB Rating: ".green + movie_detail.imdbRating);
+        if(movie_detail.Ratings && movie_detail.Ratings[1]){
+            console.log("Rotten Tomatoes Rating: ".green + movie_detail.Ratings[1].Value);
+        }else{
+            console.log("Rotten Tomatoes Rating: ".green + "N/A")
+        }
+        
+        console.log("Country: ".green + movie_detail.Country);
+        console.log("Language: ".green + movie_detail.Language);
+        console.log("Plot: ".green + movie_detail.Plot);
+        console.log("Actors: ".green + movie_detail.Actors);
+    })
 }
+
+// read the random.txt file and run the first command from the file
+function doWhatItSays(){
+    fs.readFile("random.txt","utf8",(error,data)=>{
+        if(error){
+            return console.log('Error occurred: ' + error);
+        }
+
+        var data = data.replace("\n",",").replace(/\"/g,"").split(',')
+        runCommand(data[0],data[1]);
+    })
+}
+
+// run the command with option
+function runCommand(command,option){ 
+    switch(command){
+        case "my-tweets":
+            console.log("\nmy-tweets".blue+"\n");
+            getTweets();
+            break;
+        case "spotify-this-song":
+            var song = option ? option:"The Sign";
+            console.log(("\nspotify-this-song \""+song+"\"").blue+"\n");
+            spotifySong(song);
+            break;
+        case "movie-this":
+            var movie = option ? option:"Mr. Nobody";
+            console.log(("\nmovie-this \""+movie+"\"").blue+"\n");
+            movieThis(movie);
+            break;
+        case "do-what-it-says":
+            console.log("\ndo-what-it-says".blue+"\n");
+            doWhatItSays();
+            break;
+        default:
+            console.log("Sorry, I don't understand.")
+            break;
+    }   
+}
+
+
+var command = process.argv[2]; // command from user, like "my-tweets", "spotify-this-song" and etc.
+var option = process.argv[3] // option or search key from user, like movie name, name of song
+runCommand(command,option);
